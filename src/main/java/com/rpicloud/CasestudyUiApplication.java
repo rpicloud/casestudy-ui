cccb42fdcb42be067e1bfde01fe6a4d80617ba74
Kasper Nissen <kni@it-minds.dk>
2016-02-22 22:17:04 +0100

    UI configured for remote

--- src/main/java/com/rpicloud/CasestudyUiApplication.java
+++ src/main/java/com/rpicloud/CasestudyUiApplication.java
@@ -1,10 +1,14 @@
 package com.rpicloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -44,4 +48,21 @@ public class CasestudyUiApplication {
 		}
 	}
 
+	@Autowired
+	public void setEnvironment(Environment e){
+		System.out.println(e.getProperty("configuration.projectName"));
+	}
+
+}
+
+@RestController
+@RefreshScope
+class ProjectNameRestController {
+	@Value("${configuration.projectName}")
+	String projectName;
+
+	@RequestMapping("/project-name")
+	String projectName(){
+		return this.projectName;
+	}
 }
