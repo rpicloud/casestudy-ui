47f4586f4c229b99c4c85a4881d8f827063c2484
Kasper Nissen <kni@it-minds.dk>
2016-02-22 20:58:36 +0100

    Initial commit

---
+++ src/main/java/com/rpicloud/CasestudyUiApplication.java
@@ -0,0 +1,12 @@
+package com.rpicloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasestudyUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasestudyUiApplication.class, args);
+	}
+}
